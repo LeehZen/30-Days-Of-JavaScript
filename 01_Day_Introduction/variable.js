b9caf959ae3a24dfae733bf97d8292063367630f
LeehZen <96628093+LeehZen@users.noreply.github.com>
2022-09-16 19:13:06 +0530

    changed the value of gravity to be more precise

--- 01_Day_Introduction/variable.js
+++ 01_Day_Introduction/variable.js
@@ -7,7 +7,7 @@ let age = 100 // age in years
 let isMarried = true
 
 // Declaring variables with number values
-const gravity = 9.81 // earth gravity  in m/s2
+const gravity = 9.80665 // earth gravity  in m/s2
 const boilingPoint = 100 // water boiling point, temperature in oC
 const PI = 3.14 // geometrical constant
 
